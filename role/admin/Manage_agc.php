6ec14e631280e7ce95212d24445a6dbd033b0348
paradise-j <warastudio61@gmail.com>
2023-02-23 14:04:08 +0700

    Update Manage_agc.php

--- role/admin/Manage_agc.php
+++ role/admin/Manage_agc.php
@@ -100,38 +100,12 @@
                         </div>
                     </div>
                 </div>
-
-                <!-- Footer -->
-                <footer class="sticky-footer bg-white">
-                    <div class="container my-auto">
-                        <div class="copyright text-center my-auto">
-                            <span>Copyright &copy; Your Website 2020</span>
-                        </div>
-                    </div>
-                </footer>
+                <?php include('footer.php'); ?><!-- Footer -->
             </div>
         </div>
         <a class="scroll-to-top rounded" href="#page-top">
             <i class="fas fa-angle-up"></i>
         </a>
-        <div class="modal fade" id="DeletetModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">ออกจากระบบ</h5>
-                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">คุณต้องการที่จะออกระบบหรือไม่ <?= $agc['agc_id']; ?></div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">ยกเลิก</button>
-                        <a class="btn btn-danger" href="">ออกจากระบบ</a>
-                    </div>
-                </div>
-            </div>
-        </div>
         <!-- Bootstrap core JavaScript-->
         <script src="vendor/jquery/jquery.min.js"></script>
         <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
