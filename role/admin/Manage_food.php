f1e65ac9ac3ea6cbd62d869f5525f9a63821396b
paradise-j <warastudio61@gmail.com>
2023-02-25 10:26:05 +0700

    .

--- role/admin/Manage_food.php
+++ role/admin/Manage_food.php
@@ -31,6 +31,7 @@
     <title>จัดการข้อมูลอาหาร</title>
 
     <!-- Custom fonts for this template -->
+    <link rel="icon" type="image/png" href="img/seedling-solid.svg" />
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
--- role/admin/Save_feeding.php
+++ role/admin/Save_feeding.php
@@ -14,7 +14,7 @@
     <title>บันทึกข้อมูลการให้อาหาร</title>
 
     <!-- Custom fonts for this template-->
-    <link rel="icon" type="image/png" href="img/edit_pro.png" />
+    <link rel="icon" type="image/png" href="img/seedling-solid.svg" />
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
