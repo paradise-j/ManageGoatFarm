27e3a42e66590f9c953129c864604bc3772041ef
paradise-j <warastudio61@gmail.com>
2023-02-25 11:10:55 +0700

    Update Save_feeding.php

--- role/admin/Save_feeding.php
+++ role/admin/Save_feeding.php
@@ -35,18 +35,18 @@
                     <div class="row">
                         <div class="col-lg-1"></div>
                         <div class="col-lg-10">
-                            <div class="card shadow mb-4">
+                            <div class="card shadow mb-2">
                                 <div class="card-header py-3 text-center">
                                     <h3 class="m-0 font-weight-bold text-primary">บันทึกข้อมูลการให้อาหาร</h3>
                                 </div>
                                 <div class="card-body">
-                                    <div class="row mb-4">
+                                    <!-- <div class="row mb-2">
                                         <div class="col text-center">
-                                            <img loading="lazy" width="175px" style="border-radius: 200px;" src="img/Goat.png" alt="">
+                                            <img loading="lazy" width="75px" style="border-radius: 200px;" src="img/Goat.png" alt="">
                                         </div>
-                                    </div>
+                                    </div> -->
                                     <form action="Chack_Add_agc.php" method="POST" enctype="multipart/form-data">
-                                        <div class="row mb-4">
+                                        <div class="row mb-2">
                                             <div class="col-md-2"></div>
                                             <div class="col-md-3">
                                                 <label class="form-label">ประเภท</label>
@@ -75,7 +75,7 @@
                                                 <input type="text" class="form-control" id="Fname" name="Fname" style="border-radius: 30px;" required>
                                             </div>
                                         </div>
-                                        <div class="row mb-4">
+                                        <div class="row mb-2">
                                             <div class="col-md-3"></div>
                                             <div class="col-md-3">
                                                 <label class="form-label">ราคาต่อกิโลกรัม</label>
