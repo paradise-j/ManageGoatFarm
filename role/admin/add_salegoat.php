583eb47c8282df7250d4802eda8ae2750dcb5388
paradise-j <warastudio61@gmail.com>
2023-03-13 17:08:31 +0700

    Update add_salegoat.php

--- role/admin/add_salegoat.php
+++ role/admin/add_salegoat.php
@@ -76,9 +76,8 @@
     <!-- Custom fonts for this template-->
     <link rel="icon" type="image/png" href="img/edit_pro.png" />
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Kanit:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Kanit:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
