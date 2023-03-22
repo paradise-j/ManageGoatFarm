89fd265d6db34553e334161ec5c0a99efb142d47
paradise-j <warastudio61@gmail.com>
2023-03-22 17:13:13 +0700

    Update footer.php

--- role/agriculturist/footer.php
+++ role/agriculturist/footer.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; 2023 สำนักงานสภาเกษตรกรจังหวัดสุราษฎร์ธานี</span>
+            <span>Copyright &copy; สำนักงานสภาเกษตรกรจังหวัดสุราษฎร์ธานี 2023</span>
         </div>
     </div>
 </footer>
