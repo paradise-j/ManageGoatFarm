bad488367681efe8ac687fc9d64334982c2edf52
paradise-j <warastudio61@gmail.com>
2023-03-24 16:22:28 +0700

    Update chart-area-demo.js

--- role/admin/js/demo/chart-area-demo.js
+++ role/admin/js/demo/chart-area-demo.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', async (event) => {
     data: {
       labels: ["มกราคม","กุมภาพันธ์","มีนาคม","เมษายน","พฤษภาคม","มิถุนายน","กรกฎาคม","สิงหาคม","กันยายน","ตุลาคม","พฤศจิกายน","ธันวาคม"],
       datasets: [{
-        label: "รายรับ",
+        label: "รายได้แฝง",
         lineTension: 0.3,
         backgroundColor: "rgba(78, 115, 223, 0.07)",
         borderColor: "rgba(78, 115, 223, 1)",
@@ -132,94 +132,4 @@ window.addEventListener('DOMContentLoaded', async (event) => {
       }
     }
   });
-
-  var ctx2 = document.getElementById("myAreaChart2");
-  var myLineChart = new Chart(ctx2, {
-    type: 'line',
-    data: {
-      labels: ["มกราคม","กุมภาพันธ์","มีนาคม","เมษายน","พฤษภาคม","มิถุนายน","กรกฎาคม","สิงหาคม","กันยายน","ตุลาคม","พฤศจิกายน","ธันวาคม"],
-      datasets: [{
-        label: "รายรับ",
-        lineTension: 0.3,
-        backgroundColor: "rgba(78, 115, 223, 0.07)",
-        borderColor: "rgba(78, 115, 223, 1)",
-        pointRadius: 5,
-        pointBackgroundColor: "rgba(78, 115, 223, 1)",
-        pointBorderColor: "rgba(78, 115, 223, 1)",
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-        pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-        pointHitRadius: 10,
-        pointBorderWidth: 2,
-        data: [6500, 8400, 7800, 9500, 8500, 7500, 10000, 9500, 7700, 8400, 6900, 5800],
-      }],
-    },
-    options: {
-      maintainAspectRatio: false,
-      layout: {
-        padding: {
-          left: 10,
-          right: 25,
-          top: 25,
-          bottom: 0
-        }
-      },
-      scales: {
-        xAxes: [{
-          time: {
-            unit: 'date'
-          },
-          gridLines: {
-            display: true,
-            drawBorder: false
-          },
-          ticks: {
-            maxTicksLimit: 12
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            maxTicksLimit: 5,
-            padding: 10,
-            // Include a dollar sign in the ticks
-            callback: function(value, index, values) {
-              return '$' + number_format(value);
-            }
-          },
-          gridLines: {
-            color: "rgb(234, 236, 244)",
-            zeroLineColor: "rgb(234, 236, 244)",
-            drawBorder: false,
-            borderDash: [2],
-            zeroLineBorderDash: [2]
-          }
-        }],
-      },
-      legend: {
-        display: true
-      },
-      tooltips: {
-        backgroundColor: "rgb(255,255,255)",
-        bodyFontColor: "#858796",
-        titleMarginBottom: 10,
-        titleFontColor: '#6e707e',
-        titleFontSize: 14,
-        borderColor: '#dddfeb',
-        borderWidth: 1,
-        xPadding: 15,
-        yPadding: 15,
-        displayColors: false,
-        intersect: false,
-        mode: 'index',
-        caretPadding: 10,
-        callbacks: {
-          label: function(tooltipItem, chart) {
-            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-            return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
-          }
-        }
-      }
-    }
-  });
-
 });
