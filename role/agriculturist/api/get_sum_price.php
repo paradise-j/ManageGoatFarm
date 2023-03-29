1a30512f26ca895f08b63eb752d382b5be73a005
paradise-j <warastudio61@gmail.com>
2023-03-29 17:37:32 +0700

    Update get_sum_price.php

--- role/agriculturist/api/get_sum_price.php
+++ role/agriculturist/api/get_sum_price.php
@@ -1,10 +1,18 @@
 <?php
     header('Content-Type: application/json; charset=utf-8');
+    session_start();
+    // echo '<pre>' . print_r($_SESSION, TRUE) . '</pre>';
     require_once('../connect.php');
-    $sql = "SELECT group_g.gg_type , MONTH(sale.sale_date) as month , SUM(salelist.slist_price) as total 
-            FROM `salelist` 
-            INNER JOIN `group_g` ON group_g.gg_id = salelist.gg_id INNER JOIN `sale` ON sale.sale_id = salelist.sale_id 
-            GROUP BY  group_g.gg_type , MONTH(sale.sale_date) ";
+
+    $uid = $_SESSION['id'];
+    $sql = "SELECT group_g.gg_type , MONTH(sale.sale_date) as month , SUM(salelist.slist_price) as total FROM `salelist` 
+                    INNER JOIN `group_g` ON group_g.gg_id = salelist.gg_id 
+                    INNER JOIN `sale` ON sale.sale_id = salelist.sale_id 
+                    INNER JOIN `agriculturist` ON sale.agc_id = agriculturist.agc_id 
+                    INNER JOIN `user_login` ON user_login.agc_id = agriculturist.agc_id 
+                    WHERE user_login.user_id = '$uid' 
+                    GROUP BY group_g.gg_type , MONTH(sale.sale_date) ";
+
     $result = $db->query($sql);
     $result->execute();
     // $data = $result->fetch(PDO::FETCH_ASSOC);
@@ -13,4 +21,5 @@
         $arr[] = $row;
     }
     echo json_encode($arr);
+    // print_r($arr);
 ?>
