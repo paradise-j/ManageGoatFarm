f5cbd4437e8257561d94533690dcac587f2f07a6
paradise-j <warastudio61@gmail.com>
2023-05-20 19:51:01 +0700

    update conn

--- role/director/connect.php
+++ role/director/connect.php
@@ -3,9 +3,10 @@
     $username = "motolife_surat1";
     $password = "motolife_surat1";
     $dbname = "motolife_surat1";
+    $charset="utf8mb4";
 
     try {
-        $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $db = new PDO("mysql:host=$servername;dbname=$dbname;charset=$charset", $username, $password);
         // set the PDO error mode to exception
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
